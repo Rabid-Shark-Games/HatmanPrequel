97484af8b4ea098bcb5ba56cb300d654a7220e61
ThatCreeper <creeperhostanimations@gmail.com>
2022-07-15 14:10:45 -0500

    Look at & gravity tweeks

--- ShootingBird.gd
+++ ShootingBird.gd
@@ -7,11 +7,28 @@ extends RigidBody2D
 
 var shot = false
 var mstart = null
-var shooting = true
+var shooting = false
+var collided = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	gravity_scale = 0
+	connect("body_entered", self, "on_be")
+
+func on_be(body):
+	if not shot:
+		return
+	collided = true
+	gravity_scale = 1
+	var timer = Timer.new()
+	timer.wait_time = 5
+	timer.one_shot = true
+	add_child(timer)
+	timer.start()
+	yield(timer, "timeout")
+	queue_free()
+	timer.queue_free()
+	$"..".spawn_bird()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -34,7 +51,7 @@ func shoot():
 	shot = true
 	var vec = (get_viewport().get_mouse_position() - mstart)
 	vec = vec.normalized() * min(200, vec.length())
-	gravity_scale = 1
+	gravity_scale = 5
 	linear_velocity = -Vector2(vec.x * 10, vec.y * 6)
 
 func _physics_process(delta):
@@ -42,3 +59,6 @@ func _physics_process(delta):
 		if linear_velocity.length_squared() < 100:
 			queue_free()
 			$"..".spawn_bird()
+		if not collided:
+			angular_velocity = 0
+			look_at(linear_velocity + global_position)
